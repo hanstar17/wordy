0236c5bbdb26922db64c4f93c976b8fb89b0f0b4
hanstar17 <hanstar17@gmail.com>
2019-09-15 17:58:19 -0700

    signin, auth and refreshing auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -8,12 +9,14 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-var jwtSecretKey = []byte("wordthy_secret_key_1029101")
+var jwtKey = []byte("wordthy_secret_key_1029101")
 
 const (
-	sessionTime = 5 * time.Minute
-	usernameKey = "username"
-	passwordKey = "password"
+	sessionTime   = 5 * time.Minute
+	refreshWindow = 30 * time.Second
+	usernameKey   = "username"
+	passwordKey   = "password"
+	tokenKey      = "token"
 )
 
 var users = map[string]string{
@@ -23,6 +26,15 @@ var users = map[string]string{
 type Claims struct {
 	Username string `json:"username"`
 	jwt.StandardClaims
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	claims := authenticate(r)
+	if claims != nil {
+		w.Write([]byte(fmt.Sprintf("Authed! Valid until %d", claims.ExpiresAt)))
+	} else {
+		w.Write([]byte(fmt.Sprintf("Please Sign In")))
+	}
 }
 
 func signinHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,30 +62,86 @@ func signinHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if !issueToken(w, username[0]) {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+func refreshHandler(w http.ResponseWriter, r *http.Request) {
+	claims := authenticate(r)
+	if claims == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > refreshWindow {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !issueToken(w, claims.Username) {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func authenticate(r *http.Request) *Claims {
+	cookie, err := r.Cookie(tokenKey)
+	if err != nil {
+		return nil
+	}
+
+	token := cookie.Value
+	claims := &Claims{}
+	jwtToken, err := jwt.ParseWithClaims(
+		token,
+		claims,
+		func(_ *jwt.Token) (interface{}, error) { return jwtKey, nil },
+	)
+
+	if err != nil || !jwtToken.Valid {
+		return nil
+	}
+
+	return claims
+}
+
+func issueToken(w http.ResponseWriter, username string) bool {
 	expiresAt := time.Now().Add(sessionTime)
 	claims := &Claims{
-		Username: username[0],
+		Username: username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expiresAt.Unix(),
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtSecretKey)
+	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return false
 	}
 
-	//w.WriteHeader(http.StatusOK)
 	http.SetCookie(w, &http.Cookie{
-		Name:    "session-token",
+		Name:    tokenKey,
 		Value:   tokenString,
 		Expires: expiresAt,
 	})
+	return true
+}
+
+func makeAuthed(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if authenticate(r) != nil {
+			handler(w, r)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
 }
 
 func main() {
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/signin", signinHandler)
+	http.HandleFunc("/refresh", refreshHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
